84642cbf5dbebfa06ab7588d4431179b8a4c0db0
GreerPage <greerishere@gmail.com>
2020-01-24 16:22:31 -0500

    first

---
+++ static/script.js
@@ -0,0 +1,26 @@
+function VisibilityIdToggle(idname){
+    var x = document.getElementById(idname);
+    if(window.getComputedStyle(x).visibility === "hidden"){
+        document.getElementById(idname).style.visibility = "visible"; 
+        return 0;
+    }
+    if(window.getComputedStyle(x).visibility === "visible"){
+        document.getElementById(idname).style.visibility = "hidden"; 
+        return 0;
+    }
+}
+
+function Slide(idname){
+    document.getElementById(idname).classList.toggle('closed');
+    return 0;
+}
+
+//jQuery
+$(document).ready(function() {
+    function CheckWindowWidth(){
+        if($(window).width() > 768){
+            document.getElementById('smallmenu').classList.add('closed1');
+            console.log('e');
+        }
+    }
+});
