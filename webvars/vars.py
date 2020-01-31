108472b98b8c7c4fb44c9735695e562379fa0216
GreerPage <greerishere@gmail.com>
2020-01-31 15:22:29 -0500

    updated templates

--- webvars/vars.py
+++ webvars/vars.py
@@ -18,5 +18,5 @@ medialinks = [
     "https://open.spotify.com/user/mrmcmallow?si=V0Tedh3UTA2iKW4-whJF1Q",
 ]
 
-abouttext = "This website is hosted on a Raspberry Pi 4 using apache2. I am using cloudflare's free ssl service for encryption. I originally wrote the website in html, css, and javascript, until I decided to that it would be a good idea to learn something new. So, I rewrote the website in a python web framework called Django. The code can be found on my GitHub."
+abouttext = "This website is hosted on a Raspberry Pi 4 using apache2. I am using cloudflare's free ssl service for encryption. I originally wrote the website in html, css, and javascript, until I decided to that it would be a good idea to learn something new. So, I rewrote the website in a python web framework called Django. The code can be found on my Github."
 
