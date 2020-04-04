55eb631d743aaea21857f2622be468e4cb735fe5
GreerPage <greerishere@gmail.com>
2020-04-04 16:50:44 -0400

    updated wsgi.py to update git info while site is live

--- websiteDjango/wsgi.py
+++ websiteDjango/wsgi.py
@@ -11,6 +11,16 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from webvars import git
+
+import threading
+
+def update():
+    threading.Timer(60, update).start
+    git.updateAll()
+
+update()
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'websiteDjango.settings')
 
 application = get_wsgi_application()
